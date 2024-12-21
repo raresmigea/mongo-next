581878168a93c5b8cc26f93129740a24ba7703c1
raresmigea <raresini@yahoo.com>
2024-12-22 01:50:12 +0200

    update log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import { getPosts } from '@/_actions/postAction';
 
 export default async function Home() {
   // debugger;
-  const res = await getPosts();  
-  console.log('AAA: ', res);
+  const res = await getPosts();
+  console.log('result: ', res);
   return (
     <main>
-      <h1>Hola!</h1>   
+      <h1>Hola!</h1>
     </main>
   );
 }
